96b540465493ff3006c73e6ebe9b991f8334b2cb
Daniel White <whitedann@gmail.com>
2018-12-12 21:20:06 -0800

    adding frontpage scripting

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+$('#gameMedia').html("<video id=\"vidoe\" src=\"assets/LastCircleDemo2.mp4\" style=\"text-align:center; height: 150px; width: 200px; margin: 15px; float: right;\" loop muted></video>");
+
+
+$('.post-title').mouseenter(function(){
+	$('#vidoe').get(0).play();
+});
+
+$('.post-title').mouseleave(function(){
+	$('#vidoe').get(0).pause();
+});
+
+//$('#gameMedia').html("<video src=\"assets/LastCircleDemo.mp4\" style=\"text-align:center; height: 150px; width: 200px; margin: 15px; float: right;\" loop muted autoplay=\"true\"></video>");
 
