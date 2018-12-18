80453d9bbb43c5826c80658e29a84b227a70f23e
Daniel White <whitedann@gmail.com>
2018-12-18 13:20:57 -0800

    adding video animation script to front page

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-$('#gameMedia').html("<video id=\"vidoe\" src=\"assets/LastCircleDemo2.mp4\" style=\"text-align:center; height: 150px; width: 200px; margin: 15px; float: right;\" loop muted></video>");
+$('#LastCircle').append("<video class=\"vidoe\" src=\"assets/LastCircleDemo2.mp4\" style=\"text-align:center; height: 150px; width: 200px; margin: 15px; float: right;\" loop muted></video>");
 
+$('#Pendulums').append("<video class=\"vidoe\" src=\"assets/dpend90.mp4\" style=\"height: 150px; width: 200px; margin: 15px; float: right;\" loop muted></video>");
 
-$('.post-title').mouseenter(function(){
-	$('#vidoe').get(0).play();
+$('.postEntry').on('mouseenter', function(){
+	let ele = $('.postEntry .vidoe');
+	console.log(ele);
+	$(this).find('.vidoe').get(0).play();
+	$(this).css({
+		"background-color": "rgb(210,210,220)",
+	});
 });
 
-$('.post-title').mouseleave(function(){
-	$('#vidoe').get(0).pause();
+$('.postEntry').on('mouseleave', function(){
+	$(this).find('.vidoe').get(0).pause();
+	$(this).css('background-color', 'rgb(230,230,224)');
 });
-
-//$('#gameMedia').html("<video src=\"assets/LastCircleDemo.mp4\" style=\"text-align:center; height: 150px; width: 200px; margin: 15px; float: right;\" loop muted autoplay=\"true\"></video>");
 
