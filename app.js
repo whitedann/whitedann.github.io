14625ee89be90fc3882c03948c881d3ccdb64793
Daniel White <whitedann@gmail.com>
2018-12-20 21:12:10 -0800

    testing new fornt page format

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-$('#LastCircle').append("<video class=\"vidoe\" src=\"assets/LastCircleDemo2.mp4\" style=\"text-align:center; height: 150px; width: 200px; margin: 15px; float: right;\" loop muted></video>");
+$('#LastCircle').append("<video class=\"vidoe\" src=\"assets/LastCircleDemo2.mp4\" style=\"text-align:center; height: 300px; width: 300px; margin: 15px; float: right;\" loop muted></video>");
 
-$('#Pendulums').append("<video class=\"vidoe\" src=\"assets/dpend90.mp4\" style=\"height: 150px; width: 200px; margin: 15px; float: right;\" loop muted></video>");
+$('#Pendulums').append("<video class=\"vidoe\" src=\"assets/dpend90.mp4\" style=\"height: 300px; width: 300px; margin: 15px; float: right;\" loop muted></video>");
+
+// returns true if element exists //
+$.fn.exists = function () {
+	return this.length !== 0;
+}
+
 
 $('.postEntry').on('mouseenter', function(){
-	let ele = $('.postEntry .vidoe');
-	console.log(ele);
-	$(this).find('.vidoe').get(0).play();
-	$(this).css({
-		"background-color": "rgb(210,210,220)",
-	});
+	let videoThumbnail = $(this).has('.vidoe');
+	if(videoThumbnail.exists()){
+		$(this).find('.vidoe').get(0).play();
+	}
+	//$(this).find('.post-content').hide();
+	$(this).css('background-color', '#a8a8a1');
 });
 
 $('.postEntry').on('mouseleave', function(){
-	$(this).find('.vidoe').get(0).pause();
+	let ele = $(this).has('.vidoe');
+	if(ele.exists()){
+		$(this).find('.vidoe').get(0).pause();
+	}
+	//$(this).find('.post-content').show();
 	$(this).css('background-color', 'rgb(230,230,224)');
 });
 
