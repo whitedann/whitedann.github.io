e554324a757a660465213e55f16cc9e9c87f4204
Daniel White <whitedann@gmail.com>
2018-12-24 11:29:35 -0800

    adding front page animations

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-$('#LastCircle').append("<video class=\"vidoe\" src=\"assets/LastCircleDemo2.mp4\" style=\"text-align:center; height: 300px; width: 300px; margin: 15px; float: right;\" loop muted></video>");
+$('#LastCircle').append("<video class=\"vidoe\" src=\"assets/LastCircleDemo2.mp4\" style=\"; height: 300px; width: 300px;margin-right: 15px\" loop muted></video>");
 
-$('#Pendulums').append("<video class=\"vidoe\" src=\"assets/dpend90.mp4\" style=\"height: 300px; width: 300px; margin: 15px; float: right;\" loop muted></video>");
+$('#Pendulums').append("<video class=\"vidoe\" src=\"assets/dpend90.mp4\" style=\"height: 300px; width: 300px; margin-right: 15px;\" loop muted></video>");
 
 // returns true if element exists //
 $.fn.exists = function () {
 	return this.length !== 0;
 }
 
-
 $('.postEntry').on('mouseenter', function(){
+	$(this).find('p').css("display", "block");
 	let videoThumbnail = $(this).has('.vidoe');
 	if(videoThumbnail.exists()){
 		$(this).find('.vidoe').get(0).play();
+		$(this).find('vidoe').css("opacity", "1.0");
 	}
-	//$(this).find('.post-content').hide();
+	let title = $(this).find('.post-title div');
+	title.stop();
+	title.animate({
+		marginLeft: "5%",
+		marginTop: "-20%",
+	}, 300, function() {
+		$(this).find('p').css("opacity", "1.0")
+	});
 	$(this).css('background-color', '#a8a8a1');
 });
 
@@ -21,8 +29,23 @@ $('.postEntry').on('mouseleave', function(){
 	let ele = $(this).has('.vidoe');
 	if(ele.exists()){
 		$(this).find('.vidoe').get(0).pause();
+		$(this).find('.vidoe').css("opacity", "1.0");
 	}
-	//$(this).find('.post-content').show();
+	let title = $(this).find('.post-title div');
+	title.stop();
+	$(this).find('p').css({"display": "block",
+			"opacity": "0.0"});	
+	title.animate({
+		marginLeft: "auto",
+		marginTop: "25%",
+	}, 200);
 	$(this).css('background-color', 'rgb(230,230,224)');
+});
+
+$('.postEntry').click(function(){
+	$(this).find('div').stop();
+	if($(this).has('a').length){
+		console.log(window.location.href = $(this).find("a:first").attr("href"));
+	}
 });
 
